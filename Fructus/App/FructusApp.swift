6bc3bc92bae5bbc8bde41243df0c72f3f386262a
mr-kelma <mr.kelma@gmail.com>
2022-12-13 19:15:12 +0400

    Initial commit

---
+++ Fructus/App/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Valery Keplin on 13.12.22.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
